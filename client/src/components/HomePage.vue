a81eb2dea160ff023b2e500841e52f25b17f3517
chad-tung <sven.tung@gmail.com>
2021-03-17 16:07:49 +0000

    adds homepage.vue

---
+++ client/src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <div class="title-heading">
+    <h1>Broker Insights</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home-page"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
