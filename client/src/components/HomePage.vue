2799c18d6b71d7d9509507f9e0a21831ab796acd
chad-tung <sven.tung@gmail.com>
2021-03-17 23:08:04 +0000

    added event bus

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,12 +1,21 @@
 <template lang="html">
   <div class="title-heading">
     <h1>Broker Insights</h1>
+    <p v-on:click="handleShowClick">Show Clients</p>
   </div>
 </template>
 
 <script>
+import { eventBus } from "../main.js";
+
 export default {
-  name: "home-page"
+  name: "home-page",
+
+  methods: {
+    handleShowClick() {
+      eventBus.$emit("show-clients");
+    }
+  }
 }
 </script>
 
