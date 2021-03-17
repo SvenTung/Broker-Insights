d8736b6ee965bb160ec90242bc52344f44d431ac
chad-tung <sven.tung@gmail.com>
2021-03-17 16:09:33 +0000

    adds imports

---
+++ client/src/App.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+
+  <div class="app" id="app">
+    <div class="home-page" id="home-page">
+      <home-page />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { eventBus } from "./main.js";
+import HomePage from "./components/HomePage.vue";
+
+export default {
+  name: "app",
+
+  data() {
+    return {
+      clients: []
+    };
+  },
+
+  mounted() {
+    eventBus.$on("get-policies", client => {
+      this.displayPolicies(client);
+    });
+  },
+
+  components: {
+    "home-page": HomePage
+  },
+
+  methods: {
+    displayPolicies(client) {
+      this.display = policies
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
