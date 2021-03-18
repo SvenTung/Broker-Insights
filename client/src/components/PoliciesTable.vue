016bcdc1ccf1908e1777e31ee0246a2632d991cf
chad-tung <sven.tung@gmail.com>
2021-03-18 17:41:46 +0000

    changes policies list to policies table

---
+++ client/src/components/PoliciesTable.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div class="policies-table">
+    <table>
+      <tr>
+        <th>Customer Name</th>
+        <th>Customer Address</th>
+        <th>Premium</th>
+        <th>Policy Type</th>
+        <th>Insurer Name</th>
+      </tr>
+      <policy-table-item v-for="(policy, index) in policies" :policy="policy" :counter="index"/>
+    </table>
+  </div>
+
+</template>
+
+<script>
+import PolicyTableItem from "./PolicyTableItem.vue";
+import { eventBus } from "../main.js";
+
+export default {
+  name: "policies-table",
+  props: ["policies"],
+  components: {
+    "policy-table-item": PolicyTableItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+th{
+  color: #00EAFF;
+  text-align: left;
+  padding: 0 20px;
+  margin-bottom: 20px;
+  font-family: Arial, sans-serif;
+}
+</style>
