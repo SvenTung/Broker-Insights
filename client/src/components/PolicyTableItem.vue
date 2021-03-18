42d81810f637794758981caec2bd9aaf7d75d5b5
chad-tung <sven.tung@gmail.com>
2021-03-18 17:41:31 +0000

    changed policy list item to policy table item

---
+++ client/src/components/PolicyTableItem.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+    <tr>
+      <td>{{this.policy.customer_name}}</td>
+      <td>{{this.policy.customer_address}}</td>
+      <td>£{{this.policy.premium}}</td>
+      <td>{{this.policy.policy_type}}</td>
+      <td>{{this.policy.insurer_name}}</td>
+      <button v-on:click="handleDeletePolicyClick">Delete</button>
+    </tr>
+</template>
+
+<script>
+import { eventBus } from "../main.js";
+
+export default {
+  name: "policy-table-item",
+  props:[
+    "policy",
+    "counter"
+  ],
+  methods: {
+    handleDeletePolicyClick() {
+      eventBus.$emit("policy-deleted", this.counter)
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+td {
+  padding: 3px 20px 3px;
+  border: 1px solid black;
+}
+button {
+  padding: 3px 20px 3px;
+}
+</style>
