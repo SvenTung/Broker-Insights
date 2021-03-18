b9c8fa8b1f30e984c61e7e826380382e53437378
chad-tung <sven.tung@gmail.com>
2021-03-18 17:41:04 +0000

    changed list item to client list item

---
+++ client/src/components/ClientListItem.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="client-list-item">
+
+    <div class="client-name">
+      <p>{{this.client.client.name}}</p>
+      <button v-on:click="handleShowPoliciesClick()">Show Policies</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../main.js";
+
+export default {
+  name: "client-list-item",
+  props:[
+    "client"
+  ],
+  methods: {
+    handleShowPoliciesClick() {
+      eventBus.$emit("show-policies", this.client._id);
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
