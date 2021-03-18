fb374175429c0c1c650fce0f48745156dea69d28
chad-tung <sven.tung@gmail.com>
2021-03-18 16:05:02 +0000

    adds list item import

---
+++ client/src/components/ClientsList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div class="clients-list">
+    <list-item v-for="(client, index) in this.clients" :client="client" :counter="index"/>
+  </div>
+
+</template>
+
+<script>
+import ListItem from "./ListItem.vue";
+
+export default {
+  name: "clients-list",
+  props: ["clients"],
+  components: {
+    "list-item": ListItem
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
